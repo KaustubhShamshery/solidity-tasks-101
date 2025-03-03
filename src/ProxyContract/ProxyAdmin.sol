152a3e943a4a45ecff03e9838aba9b344036bc66
Kaustubh Shamshery <10321682+KaustubhShamshery@users.noreply.github.com>
2025-03-03 21:24:42 +0800

    remove some files

--- src/ProxyContract/ProxyAdmin.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-
-contract KausProxyAdmin is ProxyAdmin {
-    constructor(address _owner) ProxyAdmin(_owner) {}
-}
